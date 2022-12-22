4cb47ab2e8418a2950f61e2f309b11f58c03aee1
Happiness-code <happinessjusticeebomah@gmail.com>
2022-12-22 13:47:21 +0100

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [units, setUnits] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnits("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnits("celsius");
+  }
+  if (units === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            ºF
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            ºC |
+          </a>{" "}
+          ºF
+        </span>
+      </div>
+    );
+  }
 }
